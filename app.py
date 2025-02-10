9e023263fe9ef3c2e887720d75a58759d2c169da
HariRagavanR <hariragavan810@gmail.com>
2025-02-10 17:48:10 +0530

    Updated to my name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Hello from Flask on Render!"}
-
+    print("Hello, This is Hari!")
+home()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
