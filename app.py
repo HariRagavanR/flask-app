976f756b95b4a5aa145f95b5732cdc463fe3e485
HariRagavanR <hariragavan810@gmail.com>
2025-02-11 16:40:40 +0530

    newnamedata

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return{"Hello, This is Hari!"}
-
+    name = {"name" : "Hello, This is Hari!"}
+    return name
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
