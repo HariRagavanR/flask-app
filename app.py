faf3c74e344ea2b6c0d4b75bc311aee3b43e30ab
HariRagavanR <hariragavan810@gmail.com>
2025-02-11 17:49:56 +0530

    new changes on code

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+
 def home():
     name = {"name" : "Hello, This is Hari!"}
-    return name
+    print(name)
+    op = input("Enter the name: ")
+    print(op.upper())
+home()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
